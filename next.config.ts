05a6009c30c157d747023931ce769b537ac230f0
HSabbatella <esabbatella@archsystemsinc.com>
2025-06-13 11:43:13 -0300

    try to fix the converter

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@sparticuz/chromium-min'],
-  },
+  serverExternalPackages: ['@sparticuz/chromium-min'],
 };
 
 export default nextConfig;
